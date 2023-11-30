04c8d9afe46cfda86cf9cf68712c1162da12d2dc
MBAREK0 <elaadraouimbarek2023@gmail.com>
2023-11-30 11:14:31 +0100

    i fixed the front end

--- Cours/cours.php
+++ Cours/cours.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="description" content="" />
 	<meta name="author" content="http://webthemez.com" />
-	<link href="css/style.css" rel="stylesheet">
+
 
 	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -32,30 +32,8 @@
 	<div id="wrapper">
 
 		<!-- start header -->
-		<header>
-			<div class="navbar navbar-default navbar-static-top">
-				<div class="container">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse"
-							data-target=".navbar-collapse">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="index.html"><img src="../img/logo.png" alt="logo" /></a>
-					</div>
-					<div class="navbar-collapse collapse ">
-						<ul class="nav navbar-nav">
-							<li class="active"><a href="home.html">Home</a></li>
-							<li><a href="cours.html">Courses</a></li>
-							<li><a href="result.html">Result</a></li>
-							<li><a href="contact.html">Contact</a></li>
-
-						</ul>
-					</div>
-				</div>
-			</div>
-		</header><!-- end header -->
+		<?php include '../nav.php'?>
+		<!-- end header -->
 		<section id="inner-headline">
 			<div class="container">
 				<div class="row">
@@ -168,87 +146,7 @@
 
 		</div>
 
-		<footer>
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-3">
-						<div class="widget">
-							<h5 class="widgetheading">Our Contact</h5>
-							<address>
-								<strong>Abovecompany Inc</strong><br>
-								JC Main Road, Near Silnile tower<br>
-								Pin-21542 NewYork US.
-							</address>
-							<p>
-								<i class="icon-phone"></i> (123) 456-789 - 1255-12584 <br>
-								<i class="icon-envelope-alt"></i> email@domainname.com
-							</p>
-						</div>
-					</div>
-					<div class="col-lg-3">
-						<div class="widget">
-							<h5 class="widgetheading">Quick Links</h5>
-							<ul class="link-list">
-								<li><a href="#">Latest Events</a></li>
-								<li><a href="#">Terms and conditions</a></li>
-								<li><a href="#">Privacy policy</a></li>
-								<li><a href="#">Career</a></li>
-								<li><a href="#">Contact us</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="col-lg-3">
-						<div class="widget">
-							<h5 class="widgetheading">Latest posts</h5>
-							<ul class="link-list">
-								<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
-								<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
-								<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="col-lg-3">
-						<div class="widget">
-							<h5 class="widgetheading">Recent News</h5>
-							<ul class="link-list">
-								<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
-								<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
-								<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div id="sub-footer">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-6">
-							<div class="copyright">
-								<p>
-									<span>&copy; Above Site All right reserved. Template By </span><a
-										href="http://webthemez.com" target="_blank">WebThemez</a>
-								</p>
-							</div>
-						</div>
-						<div class="col-lg-6">
-							<ul class="social-network">
-								<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a>
-								</li>
-								<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a>
-								</li>
-								<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a>
-								</li>
-								<li><a href="#" data-placement="top" title="Pinterest"><i
-											class="fa fa-pinterest"></i></a></li>
-								<li><a href="#" data-placement="top" title="Google plus"><i
-											class="fa fa-google-plus"></i></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-
-		</footer>
+		<?php include '../footer.php'?>
 	</div>
 	<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
 
---
+++ nav.php
@@ -0,0 +1,20 @@
+	<!-- start header -->
+    <header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    
+                    <a class="navbar-brand" href="index.html"><img src="../img/logo.png" alt="logo" /></a>
+                </div>
+                <div >
+                    <ul class="nav list-nav " >
+                        <li class="active"><a href="home.html">Home</a></li>
+                        <li><a href="cours.html">Courses</a></li>
+                        <li><a href="result.html">Result</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header><!-- end header -->
