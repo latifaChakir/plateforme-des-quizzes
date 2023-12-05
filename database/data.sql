85cf57cf0e07e8136771fc379be00d765eb61a94
unknown <chakirlatifa2001@gmail.com>
2023-12-05 18:47:42 +0100

    ajouter des modifications dans le script sql

--- database/data.sql
+++ database/data.sql
@@ -36,6 +36,8 @@ CREATE table studing_courses (
 
 CREATE table Resultat (
     id_result INT PRIMARY KEY AUTO_INCREMENT,
+    ratt_note int NULL,
+    normal_note int NULL,
     cours_note int,
     user_id int,
     cours_id int,
@@ -46,18 +48,30 @@ CREATE table Resultat (
 
 CREATE table Questions (
     qst_id INT PRIMARY KEY AUTO_INCREMENT,
-    qst_content VARCHAR(255) NOT NULL,
+    qst_content TEXT NOT NULL,
     cours_id int,
     foreign key (cours_id) references Cours(cours_id) ON UPDATE CASCADE ON DELETE CASCADE 
 );
 
 CREATE table Reponses (
     rep_id INT PRIMARY KEY AUTO_INCREMENT,
-    rep_content VARCHAR(255) NOT NULL,
-    status_qst boolean ,
+    rep1 TEXT,
+    rep2 TEXT,
+    rep3 TEXT,
+    rep4 TEXT,
+    true_rep TEXT
     qst_id int,
     foreign key (qst_id) references Questions(qst_id) ON UPDATE CASCADE ON DELETE CASCADE 
 );
+CREATE TABLE reponse_student (
+    repo_id INT PRIMARY KEY AUTO_INCREMENT,
+    qstID int,
+    content VARCHAR(255) NOT NULL,
+    ratt VARCHAR(255) not NULL,
+    foreign key (qstID) references Questions(qst_id) ON UPDATE CASCADE ON DELETE CASCADE 
+
+)
+
 
 
 CREATE table progression (
--- nav.php
+++ nav.php
@@ -19,3 +19,30 @@
             </div>
         </div>
     </header>
+
+    <?php
+    // Récupérer la couleur sélectionnée depuis la première page
+    $color = isset($_POST['changeColor']) ? $_POST['changeColor'] : 'white';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Changed Background</title>
+    <style>
+        body {
+            background-color: <?php echo $color; ?>;
+        }
+    </style>
+</head>
+<body>
+
+    <h1>Page 2</h1>
+    <p>Background color has been changed.</p>
+    
+    <a href="index.php">Back to Page 1</a>
+
+</body>
+</html>
